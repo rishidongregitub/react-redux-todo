dd9fe93c14895dfa68104205354773a3f19fee26
Rishikesh Dongre <104708547+rishidongregitub@users.noreply.github.com>
2024-09-27 15:33:08 +0000

    Todo component

--- todo-app/src/Components/Todo.jsx
+++ todo-app/src/Components/Todo.jsx
@@ -1,14 +1,43 @@
-import { BsPatchPlusFill } from "react-icons/bs";
+import { useState } from "react";
+import { BsPatchPlusFill, BsSearch } from "react-icons/bs";
+import {useDispatch} from 'react-redux'
+
 const Todo = () => {
-  return (
-    <div className='max-w-4xl mx-auto sm:mt-8 p-4 bg-gray-100 rounded'>
-        <h1 className="mt-3 mb-6 text-2xl font-bold text-center uppercase">PERSONAL TODO APP</h1>
-        <div className="flex items-center mb-4">
-            <input type="text" name="addTodoInput" id="addTodoinput" placeholder="Add Todo" className="flex-grow p-2 border-b-2 border-gray-300 focus:outline-none focus:border-blue-500"/>
-            <button className="ml-4 p-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none"><BsPatchPlusFill /></button>
+    const dispatch = useDispatch();
+
+    const [newtodoText, setNewTodoText] = useState("");
+    const [searchTerm, setSearchTerm] = useState("");
+    
+    const handleAddTodo = (text)=>{
+        dispatch(addTodo(text))
+    }
+    const handleAddTodoClick = () => {
+        if (newtodoText.trim() !== "") {
+            handleAddTodo(newtodoText.trim());
+            setNewTodoText("");
+        }
+    }
+    const handleSearchChange=(value)=>{
+        setSearchTerm(value);
+        dispatch()
+    }
+    return (
+        <div className='max-w-4xl mx-auto sm:mt-8 p-4 bg-gray-100 rounded'>
+            <h2 className="mt-3 mb-6 text-2xl font-bold text-center uppercase">PERSONAL TODO APP</h2>
+            {/*Input text and btn*/}
+            <div className="flex items-center mb-4">
+                <input value={newtodoText} onChange={(e) => setNewTodoText(e.target.value)} type="text" name="addTodoInput" id="addTodoinput" placeholder="Add Todo" className="flex-grow p-2 border-b-2 border-gray-300 focus:outline-none focus:border-blue-500" />
+                <button onClick={handleAddTodoClick} className="ml-4 p-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none"><BsPatchPlusFill /></button>
+            </div>
+            {/*Input text and btn*/}
+            <div>
+                <div className="flex items-center mb-4">
+                    <input value={searchTerm} onChange={(e)=> handleSearchChange(e.target.value)} type="text" name="addTodoInput" id="addTodoInput" placeholder="Searxh" className="flex-grow p-2 border-b-2 border-gray-300 focus:outline-none focus:border-blue-500" />
+                <button onClick={handleAddTodoClick} className="ml-4 p-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none"><BsSearch /></button>
+                </div>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Todo
